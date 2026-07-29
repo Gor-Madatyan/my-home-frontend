e187defbd673f6c9e6e6671b2830e58fdcb4b805
Gor Madatyan <gor-madatyan@proton.me>
2026-07-29 22:26:17 +0400

    request real data for portfolio

--- src/routes/portfolio/+page.ts
+++ src/routes/portfolio/+page.ts
@@ -4,7 +4,7 @@ export async function load() {
 	try {
 		const response = await axios.get('http://localhost:8080/portfolio');
 		const data = response.data;
-		const projects = (data.portfolio || []).map((item: any) => ({
+		const projects = (data.portfolio || []).map((item:{project_id:number,project_name:string,note:string}) => ({
 			project_id: item.project_id,
 			project_name: item.project_name,
 			note: item.note
